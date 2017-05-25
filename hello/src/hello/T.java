934e9a3dfea0e821c8a65261bb785df4172a36c8
dearzhaorui <s231720@studenti.polito.it>
2017-05-25 13:49:45 +0200

    add output

---
+++ hello/src/hello/T.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class T {
+
+	public static void main(String[] args) {
+		Map<String,String> map=new HashMap<>();
+		System.out.println("hello world!");   
+		
+		System.out.println("test 1");
+
+	}
+
+}
